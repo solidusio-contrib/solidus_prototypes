a621b7f8eb9209de2a31927db32b32f5f5c8fea2
Graeme Nathan <graeme@stembolt.com>
2016-10-14 10:12:54 -0700

    fixup! Inject prototype concerns

--- app/models/concerns/solidus_prototypes/product_prototype_concern.rb
+++ app/models/concerns/solidus_prototypes/product_prototype_concern.rb
@@ -13,7 +13,6 @@ module SolidusPrototypes
       # @!attribute [rw] prototype_id
       #   @return [Fixnum]
       attr_reader :prototype_id
-      attr_accessor :option_values_hash
     end
 
     def prototype_id=(value)
@@ -30,31 +29,6 @@ module SolidusPrototypes
         self.option_types = prototype.option_types
         self.taxons = prototype.taxons
       end
-    end
-
-    # Ensures option_types and product_option_types exist for keys in
-    # option_values_hash.
-    #
-    # @return [Array] the option_values
-    def ensure_option_types_exist_for_values_hash
-      return if option_values_hash.nil?
-      required_option_type_ids = option_values_hash.keys.map(&:to_i)
-      self.option_type_ids |= required_option_type_ids
-    end
-
-    # Builds variants from a hash of option types & values
-    def build_variants_from_option_values_hash
-      ensure_option_types_exist_for_values_hash
-      values = option_values_hash.values
-      values = values.inject(values.shift) { |memo, value| memo.product(value).map(&:flatten) }
-
-      values.each do |ids|
-        variants.create(
-          option_value_ids: ids,
-          price: master.price
-        )
-      end
-      save
     end
   end
 end
