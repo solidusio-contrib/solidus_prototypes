e43da890ceb72652424039def0417e2b2a6b5384
John Hawthorn <john.hawthorn@gmail.com>
2018-01-11 13:24:11 -0800

    Use factory_bot

--- lib/solidus_prototypes/factories.rb
+++ lib/solidus_prototypes/factories.rb
@@ -1,7 +1,7 @@
 require 'spree/testing_support/factories/property_factory'
 
-FactoryGirl.define do
-  factory :prototype, class: Spree::Prototype do
+FactoryBot.define do
+  factory :prototype, class: 'Spree::Prototype' do
     name 'Baseball Cap'
     properties { [create(:property)] }
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -43,7 +43,7 @@ require 'spree/testing_support/url_helpers'
 require 'solidus_prototypes/factories'
 
 RSpec.configure do |config|
-  config.include FactoryGirl::Syntax::Methods
+  config.include FactoryBot::Syntax::Methods
 
   # Infer an example group's spec type from the file location.
   config.infer_spec_type_from_file_location!
