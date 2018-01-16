bf3607030d73eb11bc5cb3819391d33f2a9734ef
John Hawthorn <john@stembolt.com>
2018-01-15 16:28:53 -0800

    Fix select_prototype_ajax on all URLs
    Previously this would fail unless it was at /admin/procucts. On other
    URLs it would generate the wrong route.

--- app/assets/javascripts/spree/backend/select_prototype_ajax.js
+++ app/assets/javascripts/spree/backend/select_prototype_ajax.js
@@ -2,9 +2,8 @@ $(document).on('change', '#product_prototype_id', function() {
   var solidus_prototypes_select = $('#product_prototype_id');
   var id = solidus_prototypes_select.val();
   if (id.length) {
-    var url = new Uri('prototypes');
-    url.setPath(url.path() + '/' + id);
-    $('#product-from-prototype').load(url.toString());
+    var url = Spree.pathFor("admin") + "/prototypes/" + id;
+    $('#product-from-prototype').load(url);
   } else {
     $('#product-from-prototype').empty();
   }
