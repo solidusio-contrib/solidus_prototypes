e884be70a3e77bbb6d1767a8e36fba8f427e39f5
Pablo Castelnovo <pablonvz@gmail.com>
2018-04-19 09:29:38 -0400

    Avoid comma after the last parameter of a method call

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -2,7 +2,7 @@ Spree::Backend::Config.configure do |config|
   config.menu_items << config.class::MenuItem.new(
     [:prototypes],
     'square',
-    condition: -> { can?(:admin, Spree::Prototype) },
+    condition: -> { can?(:admin, Spree::Prototype) }
   )
 end
 
