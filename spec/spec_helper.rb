01356f836a9369b52301b7a96a3819a77b36fedd
Alberto Vena <kennyadsl@gmail.com>
2018-05-02 11:25:25 +0200

    Use webrick as Capybara server
    fix https://github.com/rspec/rspec-rails/issues/1882

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,8 @@ Dir[File.join(File.dirname(__FILE__), "support/**/*.rb")].each { |f| require f }
 # Requires factories defined in lib/solidus_prototypes/factories.rb
 require 'solidus_prototypes/factories'
 
+Capybara.server = :webrick
+
 RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   config.raise_errors_for_deprecations!
