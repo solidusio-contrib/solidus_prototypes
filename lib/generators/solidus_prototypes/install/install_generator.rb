f549be1fcaa37e739272c70a88c61ed53dc103b8
Mattia Roccoberton <mat@blocknot.es>
2020-01-23 15:58:52 +0100

    Upgrade the extension using solidus_dev_support

--- lib/generators/solidus_prototypes/install/install_generator.rb
+++ lib/generators/solidus_prototypes/install/install_generator.rb
@@ -6,13 +6,29 @@ module SolidusPrototypes
       class_option :auto_run_migrations, type: :boolean, default: false
 
       def add_javascripts
-        append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require spree/frontend/solidus_prototypes\n"
-        append_file 'vendor/assets/javascripts/spree/backend/all.js', "//= require spree/backend/solidus_prototypes\n"
+        append_file(
+          'vendor/assets/javascripts/spree/frontend/all.js',
+          "//= require spree/frontend/solidus_prototypes\n"
+        )
+        append_file(
+          'vendor/assets/javascripts/spree/backend/all.js',
+          "//= require spree/backend/solidus_prototypes\n"
+        )
       end
 
       def add_stylesheets
-        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/solidus_prototypes\n", before: %r{\*/}, verbose: true
-        inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require spree/backend/solidus_prototypes\n", before: %r{\*/}, verbose: true
+        inject_into_file(
+          'vendor/assets/stylesheets/spree/frontend/all.css',
+          " *= require spree/frontend/solidus_prototypes\n",
+          before: %r{\*/},
+          verbose: true
+        )
+        inject_into_file(
+          'vendor/assets/stylesheets/spree/backend/all.css',
+          " *= require spree/backend/solidus_prototypes\n",
+          before: %r{\*/},
+          verbose: true
+        )
       end
 
       def add_migrations
@@ -20,11 +36,13 @@ module SolidusPrototypes
       end
 
       def run_migrations
-        run_migrations = options[:auto_run_migrations] || ['', 'y', 'Y'].include?(ask('Would you like to run the migrations now? [Y/n]'))
+        run_migrations = options[:auto_run_migrations] || ['', 'y', 'Y'].include?(
+          ask('Would you like to run the migrations now? [Y/n]')
+        )
         if run_migrations
           run 'bundle exec rake db:migrate'
         else
-          puts 'Skipping rake db:migrate, don\'t forget to run it!'
+          puts 'Skipping rake db:migrate, don\'t forget to run it!' # rubocop:disable Rails/Output
         end
       end
     end
--- solidus_prototypes.gemspec
+++ solidus_prototypes.gemspec
@@ -10,15 +10,28 @@ Gem::Specification.new do |s|
   s.description = 'Breaking out prototypes from solidus'
   s.license     = 'BSD-3-Clause'
 
+  s.required_ruby_version = '~> 2.4'
+
   s.author    = 'Graeme Nathan'
   s.email     = 'graeme@stembolt.com'
   s.homepage  = 'https://github.com/gevann'
 
-  s.files = Dir["{app,config,db,lib}/**/*", 'LICENSE', 'Rakefile', 'README.md']
-  s.test_files = Dir['test/**/*']
+  s.files = Dir.chdir(File.expand_path(__dir__)) do
+    `git ls-files -z`.split("\x0").reject { |f| f.match(%r{^(test|spec|features)/}) }
+  end
+  s.test_files = Dir['spec/**/*']
+  s.bindir = "exe"
+  s.executables = s.files.grep(%r{^exe/}) { |f| File.basename(f) }
+  s.require_paths = ["lib"]
+
+  if s.respond_to?(:metadata)
+    s.metadata["homepage_uri"] = s.homepage if s.homepage
+    s.metadata["source_code_uri"] = s.homepage if s.homepage
+  end
 
   s.add_dependency 'deface'
-  s.add_dependency 'solidus_core', ['>= 2.1.0.alpha', '< 3']
+  s.add_dependency 'solidus_core', ['>= 2.1.0', '< 3']
+  s.add_dependency 'solidus_support', '~> 0.4.0'
 
   s.add_development_dependency 'solidus_dev_support'
 end
