8f77a6f046f857e2b34d8be8f853d6928d26e7e5
benjamin wil <benjamin@super.gd>
2022-09-21 16:40:52 -0700

    Keep "Products" submenu open
    When on the "Prototypes" admin page, the products submenu should remain
    open. This commit ensures that the submenu is registered properly.

--- app/overrides/decorate_admin_product_tabs.rb
+++ app/overrides/decorate_admin_product_tabs.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
+Spree::Backend::Config.configure do |config|
+  config.menu_items.detect { |menu_item|
+    menu_item.label == :products
+  }.sections << :prototypes
+end
+
 Deface::Override.new(
   virtual_path: "spree/admin/shared/_product_sub_menu",
-  name: "prototypes_product_tabs",
+  name: "prototypes_admin_tab",
   insert_bottom: "[data-hook='admin_product_sub_tabs']",
-  disabled: false,
-  partial: "spree/shared/prototypes_products_sub_menu_tab"
+  text: "<%= tab :prototypes if can?(:admin, Spree::Prototype) %>"
 )
--- spec/features/admin/prototypes_homepage_spec.rb
+++ spec/features/admin/prototypes_homepage_spec.rb
@@ -12,7 +12,28 @@ describe "Homepage with prototypes extension", type: :feature do
       end
 
       it "has a link to prototypes" do
-        within('.selected .admin-subnav') { page.find_link("Prototypes")['/admin/prototypes'] }
+        products_submenu = find ".selected .admin-subnav"
+
+        within products_submenu do
+          click_on "Prototypes"
+        end
+
+        within "#content-header" do
+          expect(page).to have_text "Prototypes"
+        end
+
+        within products_submenu do
+          expect(page).to have_text(
+            %w{
+              Products
+              Option Types
+              Property Types
+              Taxonomies
+              Display Order
+              Prototypes
+            }.join(" ")
+          )
+        end
       end
     end
   end
