b1c914ece8a9d33d5705c4cad97d02babde4e5bd
Thomas von Deyen <thomas@vondeyen.com>
2023-09-26 09:17:17 +0200

    Do not require solidus factories
    The factories are available in the load path
    and must not be required anymore.

--- lib/solidus_prototypes/testing_support/factories.rb
+++ lib/solidus_prototypes/testing_support/factories.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'spree/testing_support/factories/property_factory'
-
 FactoryBot.define do
   factory :prototype, class: 'Spree::Prototype' do
     name { 'Baseball Cap' }
