3ed7ba59e5bc690162d0a7de1106ddcc4299c009
Sascha Karnatz <68833+kulturbande@users.noreply.github.com>
2024-04-15 18:48:30 +0200

    Replace Deface Override with Deface DSL
    The override objects don't work great in a combination of Rails 7.1 and Zeitwerk. Also the html.erb.deface can contain the partial itself and it is possible to remove the additional partials.

--- app/overrides/decorate_product_properties_form.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-Deface::Override.new(
-  virtual_path: "spree/admin/product_properties/index",
-  name: "prototype_product_properties_form",
-  insert_after: "[data-hook='add_product_properties']",
-  disabled: false,
-  text: "<div id='prototypes' data-hook></div>"
-)
--- app/overrides/decorate_product_properties_index.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-Deface::Override.new(
-  virtual_path: "spree/admin/product_properties/index",
-  name: "prototypes_product_properties_toolbar",
-  insert_bottom: "ul.tollbar",
-  disabled: false,
-  partial: "spree/shared/prototypes_product_properties_toolbar"
-)
